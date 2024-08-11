750680d81ce59720bc11d154e3ff02c46e9a98e5
juan9519 <juanrod9519@icloud.com>
2024-08-11 20:53:17 -0300

    Initial Commit

---
+++ ShoppingList/ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList
+//
+//  Created by Juan Rodriguez on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
